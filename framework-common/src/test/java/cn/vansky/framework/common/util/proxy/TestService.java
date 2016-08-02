29e97e6495361cf264f52315adc23f1e40f78292
yuqiangcui <yuqiang.cui@gmail.com>
2016-08-02 16:16:26 +0800

    代理公共类优化及测试

---
+++ framework-common/src/test/java/cn/vansky/framework/common/util/proxy/TestService.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2016 CK, Inc. All Rights Reserved.
+ */
+
+package cn.vansky.framework.common.util.proxy;
+
+/**
+ * Created by IntelliJ IDEA .
+ * Auth: CK
+ * Date: 2016/8/2
+ */
+public interface TestService {
+
+    void test();
+}
