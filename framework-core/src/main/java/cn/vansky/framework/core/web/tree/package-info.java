0a8a893feedee962c284bea24d4e0bc6a38bedf8
yuqiangcui <yuqiang.cui@gmail.com>
2016-08-27 16:40:07 +0800

    添加ztree控制树结构

---
+++ framework-core/src/main/java/cn/vansky/framework/core/web/tree/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2016 CK, Inc. All Rights Reserved.
+ */
+
+/**
+ * Created by IntelliJ IDEA .
+ * Auth: CK
+ * Date: 2016/8/27
+ */
+package cn.vansky.framework.core.web.tree;
---
+++ framework-core/src/main/java/cn/vansky/framework/core/web/tree/simple/SimpleTreeServiceImpl.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2016 CK, Inc. All Rights Reserved.
+ */
+
+package cn.vansky.framework.core.web.tree.simple;
+
+import cn.vansky.framework.common.util.JsonUtil;
+import cn.vansky.framework.core.web.easyUI.model.SimpleTreeModel;
+import cn.vansky.framework.core.web.tree.SimpleTreeService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 简单树结构实现
+ * Auth: CK
+ * Date: 2016/8/27
+ */
+public class SimpleTreeServiceImpl<T> implements SimpleTreeService<T> {
+    @Override
+    public String findTreeStr(List<T> list, ModelCall<T> mc) {
+        return JsonUtil.toJson(findTree(list, mc));
+    }
+
+    @Override
+    public List<SimpleTreeModel> findTree(List<T> list, ModelCall<T> mc) {
+        List<SimpleTreeModel> simpleTreeModels = new ArrayList<SimpleTreeModel>(list.size());
+        for (T t : list){
+            simpleTreeModels.add(mc.convert(t));
+        }
+        return simpleTreeModels;
+    }
+}
