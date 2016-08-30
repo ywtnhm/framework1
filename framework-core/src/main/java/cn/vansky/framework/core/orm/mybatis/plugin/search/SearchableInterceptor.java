4cd17cdc55fdc5c7e207f4cc736f3d705a572c87
dezhanjie <dezhanjie@163.com>
2016-08-30 19:52:09 +0800

    合并拦截器

--- framework-core/src/main/java/cn/vansky/framework/core/orm/mybatis/plugin/search/SearchableInterceptor.java
+++ framework-core/src/main/java/cn/vansky/framework/core/orm/mybatis/plugin/search/SearchableInterceptor.java
@@ -1,6 +1,6 @@
 package cn.vansky.framework.core.orm.mybatis.plugin.search;
 
-import cn.vansky.framework.common.entity.search.Searchable;
+import cn.vansky.framework.core.orm.mybatis.plugin.search.entity.search.Searchable;
 import cn.vansky.framework.core.orm.mybatis.plugin.page.BaseInterceptor;
 import cn.vansky.framework.core.orm.mybatis.plugin.page.PaginationInterceptor;
 import cn.vansky.framework.core.orm.mybatis.plugin.page.SQLHelp;
@@ -21,17 +21,17 @@ import java.util.Properties;
 /**
  * @author hassop
  * @Description 查询插件 根据searchble接口的查询条件动态调整查询sql完成查询（
- *包括了条件、排序和分页条件）
- * @date 2016/7/28 0028
+ * 包括了条件、排序和分页条件）
+ * @date 2016/7/28
  */
-@Intercepts({ @Signature(type = Executor.class, method = "query",
-        args = { MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class }) })
+@Intercepts({@Signature(type = Executor.class, method = "query",
+        args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class})})
 public class SearchableInterceptor extends BaseInterceptor {
     public Object intercept(Invocation invocation) throws Throwable {
         MappedStatement mappedStatement = (MappedStatement) invocation.getArgs()[0];
-        if(mappedStatement.getId().matches(_SQL_PATTERN_Search)){
+        if (mappedStatement.getId().matches(_SQL_PATTERN_Search)) {
             Object parameter = invocation.getArgs()[1];
-            if(!Searchable.class.isAssignableFrom(parameter.getClass())){
+            if (!Searchable.class.isAssignableFrom(parameter.getClass())) {
                 invocation.proceed();
             }
             BoundSql boundSql = mappedStatement.getBoundSql(parameter);
@@ -46,7 +46,7 @@ public class SearchableInterceptor extends BaseInterceptor {
                 DIALECT = DialectType.getDialect(databaseId);
             }
             //parameter is searhabel
-            String realSql = SQLHelp.generateRealSql(originalSql, (Searchable)parameter, DIALECT);
+            String realSql = SQLHelp.generateRealSql(originalSql, (Searchable) parameter, DIALECT);
             log.info("最终 的sql为:" + realSql);
             invocation.getArgs()[2] = new RowBounds(RowBounds.NO_ROW_OFFSET, RowBounds.NO_ROW_LIMIT);
             BoundSql newBoundSql = SQLHelp.createNewBoundSql(mappedStatement, boundSql.getParameterObject(),
@@ -56,14 +56,6 @@ public class SearchableInterceptor extends BaseInterceptor {
             invocation.getArgs()[0] = newMs;
 
         }
-             return invocation.proceed();
+        return invocation.proceed();
     }
-
-    public Object plugin(Object target) {
-        return Plugin.wrap(target, this);
-    }
-
-    public void setProperties(Properties properties) {
-
-    }
-}
+ }
