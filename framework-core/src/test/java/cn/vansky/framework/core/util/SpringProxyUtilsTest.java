599020fc660cdfe7f1826fc724140bf690d498b4
dezhanjie <dezhanjie@163.com>
2016-09-12 17:22:34 +0800

    测试类找回

--- framework-core/src/test/java/cn/vansky/framework/core/util/SpringProxyUtilsTest.java
+++ framework-core/src/test/java/cn/vansky/framework/core/util/SpringProxyUtilsTest.java
@@ -4,6 +4,7 @@
 
 package cn.vansky.framework.core.util;
 
+import cn.vansky.framework.core.dao.ConfigurableBaseSqlMapDao;
 import org.springframework.aop.Advisor;
 import org.springframework.aop.TargetSource;
 import org.springframework.aop.framework.ProxyFactory;
@@ -66,5 +67,93 @@ public class SpringProxyUtilsTest {
         Audi proxy = (Audi) proxyFactory.getProxy();
         Audi obj = SpringProxyUtils.getRealTarget(proxy);
         Assert.assertEquals(audi, obj);
+    }
+
+    @Test
+    public void isMultipleProxy(){
+        //cglib代理
+        Bmw bmw = new Bmw();
+        ProxyFactory proxyFactory1 = new ProxyFactory();
+        TargetSource targetSource1 = new SingletonTargetSource(bmw);
+        proxyFactory1.setTargetSource(targetSource1);
+
+        Advisor[] advisors = new Advisor[1];
+        advisors = new Advisor[1];
+        advisors[0] = advisorAdapterRegistry.wrap(new TestMethodInterceptor());
+        proxyFactory1.addAdvisors(advisors);
+        //jdk代理
+        Object object =  proxyFactory1.getProxy();
+
+        ProxyFactory proxyFactory = new ProxyFactory();
+        TargetSource targetSource = new SingletonTargetSource(object);
+        proxyFactory.setTargetSource(targetSource);
+
+        Class<?>[] targetInterfaces = ClassUtils.getAllInterfacesForClass(bmw.getClass());
+        for (Class<?> targetInterface : targetInterfaces) {
+            proxyFactory.addInterface(targetInterface);
+        }
+
+        advisors[0] = advisorAdapterRegistry.wrap(new TestMethodInterceptor());
+        proxyFactory.addAdvisors(advisors);
+
+        Car proxy = (Car) proxyFactory.getProxy();
+        //双向代理验证
+        System.out.println(SpringProxyUtils.isMultipleProxy(proxy));
+        System.out.println(SpringProxyUtils.getRealTarget(proxy));
+    }
+
+    @Test
+    public void testGetRealTarget() throws Exception {
+        //cglib代理
+        Bmw bmw = new Bmw();
+        ProxyFactory proxyFactory1 = new ProxyFactory();
+        TargetSource targetSource1 = new SingletonTargetSource(bmw);
+        proxyFactory1.setTargetSource(targetSource1);
+
+        Advisor[] advisors = new Advisor[1];
+        advisors = new Advisor[1];
+        advisors[0] = advisorAdapterRegistry.wrap(new TestMethodInterceptor());
+        proxyFactory1.addAdvisors(advisors);
+        //jdk代理
+        Object object =  proxyFactory1.getProxy();
+
+        ProxyFactory proxyFactory = new ProxyFactory();
+        TargetSource targetSource = new SingletonTargetSource(object);
+        proxyFactory.setTargetSource(targetSource);
+
+        Class<?>[] targetInterfaces = ClassUtils.getAllInterfacesForClass(bmw.getClass());
+        for (Class<?> targetInterface : targetInterfaces) {
+            proxyFactory.addInterface(targetInterface);
+        }
+
+        advisors[0] = advisorAdapterRegistry.wrap(new TestMethodInterceptor());
+        proxyFactory.addAdvisors(advisors);
+
+        Car proxy = (Car) proxyFactory.getProxy();
+        Object target = SpringProxyUtils.getRealTarget(proxy);
+        Assert.assertEquals(bmw,target);
+        Assert.assertNotEquals(bmw, SpringProxyUtils.getRealTarget(proxy));
+    }
+
+    @Test
+    public void testFindJdkDynamicProxyFactory() throws Exception {
+        ProxyFactory proxyFactory = new ProxyFactory();
+
+        Bmw bmw = new Bmw();
+        TargetSource targetSource = new SingletonTargetSource(bmw);
+        proxyFactory.setTargetSource(targetSource);
+
+        Class<?>[] targetInterfaces = ClassUtils.getAllInterfacesForClass(bmw.getClass());
+        for (Class<?> targetInterface : targetInterfaces) {
+            proxyFactory.addInterface(targetInterface);
+        }
+
+        Advisor[] advisors = new Advisor[1];
+        advisors[0] = advisorAdapterRegistry.wrap(new TestMethodInterceptor());
+        proxyFactory.addAdvisors(advisors);
+
+        Car proxy = (Car) proxyFactory.getProxy();
+        SpringProxyUtils.findJdkDynamicProxyFactory(proxy);
+
     }
 }
